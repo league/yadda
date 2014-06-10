65f4f566ea1815120de391e34bfb38daef59f4cc
Christopher League <league@contrapunctus.net>
2014-06-09 22:12:15 -0400

    Receive hook: better way to save/show build log

--- yadda/docker.py
+++ yadda/docker.py
@@ -1,10 +1,13 @@
-# yadda.docker ▪ coding: utf8
+# yadda.docker ▪ Python interface to docker commands ▪ coding: utf8
 # ©2014 Christopher League <league@contrapunctus.net>
 
+from uuid import uuid4 as uuid
 from yadda.models import Build
+from yadda.settings import HASH_ABBREV
 from yadda.utils import save_cwd, dry_call
 import os
 import subprocess
+import tempfile
 
 def build(opts, b):
     assert(isinstance(b, Build))
@@ -12,7 +15,13 @@ def build(opts, b):
         os.chdir(b.workdir)
         if b.app.subdir and os.path.isdir(b.app.subdir):
             os.chdir(b.app.subdir)
-        result = dry_call(opts, ['docker', 'build', '-t', b.tag(), '.'],
-                          call=subprocess.check_output)
-        b.build_log = result
-        b.app.save()
+        t = b.tag()
+        log = os.path.join(tempfile.gettempdir(),
+                           '%s.%s' % (t, uuid().hex[:HASH_ABBREV]))
+        try:
+            dry_call(opts, 'docker build -t %s . | tee %s' % (t, log), shell=True)
+            with open(log, 'r') as h:
+                b.build_log = h.read()
+                b.app.save()
+        finally:
+            os.unlink(log)
